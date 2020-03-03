0141418e59386d7eb4f1a2820a5ae38617fea32d
KASMI <contact@aniskasmi.com>
2020-03-03 10:14:17 +0100

    Add users to db

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,16 +1,38 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Cultures;
+use App\Repository\CulturesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController {
+
+    /**
+     * @var CulturesRepository
+     */
+    private $repository;
+
+    public function __construct(CulturesRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     /**
      * @Route("/", name="home")
      * @return Response
      */
     public function index(): Response {
+        /* INSERT QUERY
+        $cultures = new Cultures();
+        $cultures->setName('Salut');
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($cultures);
+        $em->flush();
+        */
+        $culture = $this->repository->find(1);
+        dump($culture);
         return $this->render('pages/home.html.twig');
     }
 }
---
+++ src/Entity/Users.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
+ */
+class Users
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=30, nullable=true)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $last_activity;
+
+    /**
+     * @ORM\Column(type="string", length=30, nullable=true)
+     */
+    private $certification_phyto;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getLastActivity(): ?\DateTimeInterface
+    {
+        return $this->last_activity;
+    }
+
+    public function setLastActivity(?\DateTimeInterface $last_activity): self
+    {
+        $this->last_activity = $last_activity;
+
+        return $this;
+    }
+
+    public function getCertificationPhyto(): ?string
+    {
+        return $this->certification_phyto;
+    }
+
+    public function setCertificationPhyto(?string $certification_phyto): self
+    {
+        $this->certification_phyto = $certification_phyto;
+
+        return $this;
+    }
+}
