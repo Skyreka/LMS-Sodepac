c3bae05eb6d745ad5f39ae54faa64be2fd499ab3
Théo <theodore.turpin@outlook.fr>
2020-11-05 18:11:11 +0100

    feat: optimisation ProductController

--- src/Controller/Management/ProductController.php
+++ src/Controller/Management/ProductController.php
@@ -1,32 +1,24 @@
 <?php
-namespace App\Controller;
+namespace App\Controller\Management;
 
 use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ProductController
+ * @package App\Controller\Management
+ * @Route("/management/products")
+ */
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/admin/products", name="admin.products")
+     * @Route("/", name="management_products")
      * @param ProductsRepository $pd
      * @return Response
      */
     public function admin( ProductsRepository $pd): Response
-    {
-        $products = $pd->findAll();
-        return $this->render('products/index.html.twig', [
-            'products' => $products
-        ]);
-    }
-
-    /**
-     * @Route("/technician/products", name="technician.products")
-     * @param ProductsRepository $pd
-     * @return Response
-     */
-    public function technician( ProductsRepository $pd): Response
     {
         $products = $pd->findAll();
         return $this->render('products/index.html.twig', [
