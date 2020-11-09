6743a4ff03696201236896d5fbe9d17893829de0
Théo <theodore.turpin@outlook.fr>
2020-11-09 10:59:30 +0100

    fix:canevas prune d'ente methodes alternatives
    id: 8kvyu1Kq

--- src/Migrations/Version20201108180433.php
+++ src/Migrations/Version20201108180433.php
@@ -20,16 +20,16 @@ final class Version20201108180433 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE exploitation ADD CONSTRAINT FK_BEBCFB167B3B43D FOREIGN KEY (users_id) REFERENCES users (id)');
-        $this->addSql('ALTER TABLE ilots ADD CONSTRAINT FK_E224BAF3D967A16D FOREIGN KEY (exploitation_id) REFERENCES exploitation (id)');
+        //$this->addSql('ALTER TABLE exploitation ADD CONSTRAINT FK_BEBCFB167B3B43D FOREIGN KEY (users_id) REFERENCES users (id)');
+        //$this->addSql('ALTER TABLE ilots ADD CONSTRAINT FK_E224BAF3D967A16D FOREIGN KEY (exploitation_id) REFERENCES exploitation (id)');
         $this->addSql('ALTER TABLE recommendations ADD pdf VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE exploitation DROP FOREIGN KEY FK_BEBCFB167B3B43D');
-        $this->addSql('ALTER TABLE ilots DROP FOREIGN KEY FK_E224BAF3D967A16D');
+        //$this->addSql('ALTER TABLE exploitation DROP FOREIGN KEY FK_BEBCFB167B3B43D');
+        //$this->addSql('ALTER TABLE ilots DROP FOREIGN KEY FK_E224BAF3D967A16D');
         $this->addSql('ALTER TABLE recommendations DROP pdf');
     }
 }
