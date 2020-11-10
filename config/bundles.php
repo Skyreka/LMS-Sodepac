c8bdf2324077db335d421099148070c80333abe4
KASMI Anis <contact@aniskasmi.com>
2020-11-10 23:13:47 +0100

    Add: AppFixtures

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     DataTables\DataTablesBundle::class => ['all' => true],
     Tetranz\Select2EntityBundle\TetranzSelect2EntityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
