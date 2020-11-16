61fe4d9fd8635380045c34851d72fc2e17f3c609
Théo <theodore.turpin@outlook.fr>
2020-11-16 13:44:46 +0100

    feat: datatable optimisation products

--- src/Controller/Management/ProductController.php
+++ src/Controller/Management/ProductController.php
@@ -2,7 +2,10 @@
 namespace App\Controller\Management;
 
 use App\Repository\ProductsRepository;
+use DataTables\DataTablesInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -20,9 +23,24 @@ class ProductController extends AbstractController
      */
     public function admin( ProductsRepository $pd): Response
     {
-        $products = $pd->findAll();
-        return $this->render('products/index.html.twig', [
-            'products' => $products
-        ]);
+        return $this->render('products/index.html.twig');
+    }
+
+    /**
+     * @Route("/data", name="management_products_data", methods={"GET"})
+     * @param Request $request
+     * @param DataTablesInterface $datatables
+     * @return JsonResponse
+     */
+    public function data(Request $request, DataTablesInterface $datatables): JsonResponse
+    {
+        try {
+            $results = $datatables->handle($request, 'products');
+
+            return $this->json($results);
+        }
+        catch (HttpException $e) {
+            return $this->json($e->getMessage(), $e->getStatusCode());
+        }
     }
 }
