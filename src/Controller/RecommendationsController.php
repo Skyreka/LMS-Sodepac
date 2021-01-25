488480d36168d4188a2e40f6276b2cd1fc51b9ec
Théo <theodore.turpin@outlook.fr>
2021-01-25 16:07:26 +0100

    fix: canevas add other product

--- src/Controller/RecommendationsController.php
+++ src/Controller/RecommendationsController.php
@@ -277,7 +277,7 @@ class RecommendationsController extends AbstractController
             }
             //$recommendationProducts->setUnit( $dose->getUnit() );
             // Auto Calc dose x total size
-            $recommendationProducts->setQuantity( $recommendations->getCultureSize() * floor($dose-getDose() * 1000) / 1000 );
+            $recommendationProducts->setQuantity( $recommendations->getCultureSize() * floor($dose->getDose() * 1000) / 1000 );
 
             $this->em->persist( $recommendationProducts );
             $this->em->flush();
