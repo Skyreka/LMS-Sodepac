587e6f5ab9cfbc448195fa79dfe6ee2fc602581d
KASMI Anis <contact@aniskasmi.com>
2021-07-15 23:28:16 +0200

    Fix: export users of technician

--- src/Controller/Admin/TechnicianController.php
+++ src/Controller/Admin/TechnicianController.php
@@ -56,4 +56,73 @@ class TechnicianController extends AbstractController {
             'technician' => $technician
         ]);
     }
+
+    /**
+     * @Route("/export/{id}", name="admin_technicians_export", methods={"GET", "POST"})
+     * @return Response
+     * @throws \PhpOffice\PhpSpreadsheet\Exception
+     */
+    public function export( Users $technician ): Response
+    {
+        $spreadsheet = new Spreadsheet();
+
+        $sheet = $spreadsheet->getActiveSheet();
+
+        $sheet->setTitle('Utilisateurs Tech '. $technician->getFirstname() );
+
+        $sheet->getCell('A1')->setValue('Informations');
+        $sheet->getCell('B1')->setValue('Entreprise');
+        $sheet->getCell('C1')->setValue('Adresse');
+        $sheet->getCell('D1')->setValue('Email');
+        $sheet->getCell('E1')->setValue('Téléphone');
+        $sheet->getCell('F1')->setValue('Ville');
+        $sheet->getCell('G1')->setValue('Pack');
+
+        $sheet->fromArray($this->getData( $technician ), null, 'A2', true);
+
+        $writer = new Xlsx($spreadsheet);
+
+        $response =  new StreamedResponse(
+            function () use ($writer) {
+                $writer->save('php://output');
+            }
+        );
+        $response->headers->set('Content-Type', 'application/vnd.ms-excel');
+        $response->headers->set('Content-Disposition', 'attachment;filename="Utilisateurs.xls"');
+        $response->headers->set('Cache-Control','max-age=0');
+        return $response;
+    }
+
+    private function getData( Users $technician ): array
+    {
+        $list= [];
+        $users = $this->em->getRepository(Users::class)->findByTechnician( $technician );
+
+        foreach ($users as $user) {
+            switch ($user->getPack()) {
+                case 'PACK_FULL':
+                    $pack = 'PACK FULL';
+                    break;
+                case 'PACK_LIGHT':
+                    $pack = 'PACK LIGHT';
+                    break;
+                case 'PACK_DEMO':
+                    $pack = 'PACK DEMO';
+                    break;
+                default:
+                    $pack = 'INACTIF';
+                    break;
+            }
+            $list[] = [
+                $user->getIdentity(),
+                $user->getCompany(),
+                $user->getAddress() . ' ' . $user->getPostalCode() . ' ' . $user->getCity(),
+                $user->getEmail(),
+                $user->getPhone(),
+                $user->getCity(),
+                $pack
+            ];
+        }
+        return $list;
+    }
 }
