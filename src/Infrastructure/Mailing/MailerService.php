5044a83a94b664e9fd0edb6bbd0fb5760788b9bf
Anis KASMi <contact@aniskasmi.com>
2024-02-23 15:25:40 +0100

    Remove dkim

--- src/Infrastructure/Mailing/MailerService.php
+++ src/Infrastructure/Mailing/MailerService.php
@@ -22,21 +22,21 @@ class MailerService
     )
     {
     }
-    
+
     public function createEmail(string $template, array $data = []): Email
     {
         $this->twig->addGlobal('format', 'html');
         $html = $this->twig->render($template, array_merge($data, ['layout' => 'mails/base.html.twig']));
-        
+
         $this->twig->addGlobal('format', 'text');
         $text = $this->twig->render($template, array_merge($data, ['layout' => 'mails/base.text.twig']));
-        
+
         return (new Email())
             ->from(new Address('ne-pas-repondre@sodepac.fr', 'LMS Sodepac'))
             ->html($html)
             ->text($text);
     }
-    
+
     /**
      * Enqueue send basic email
      */
@@ -44,18 +44,18 @@ class MailerService
     {
         $this->enqueue->enqueue(self::class, 'sendNow', [$email]);
     }
-    
+
     /**
      * Send email directly without enqueue
      */
     public function sendNow(Email $email): void
     {
-        if($this->dkimKey) {
+        /*if($this->dkimKey) {
             $dkimSign = new DkimSigner("file://{$this->dkimKey}", 'sodepac.fr', $this->dkimSelector);
             // Sign manualy wait fix https://github.com/symfony/symfony/issues/40131
             $message = new Message($email->getPreparedHeaders(), $email->getBody());
             $email   = $dkimSign->sign($message);
-        }
+        }*/
         $this->mailer->send($email);
     }
 }
